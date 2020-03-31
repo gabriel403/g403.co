580a5dc476bb1fc6fc5aa69b5d84b686bdc68a1f
Gabriel Baker <me@g403.co>
2020-03-31 08:18:04 +0100

    Possibly PWA

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+// service-worker.js
+
+// set names for both precache & runtime cache
+workbox.core.setCacheNameDetails({
+  prefix: "g403-blog",
+  suffix: "v1",
+  precache: "precache",
+  runtime: "runtime-cache"
+});
+
+// let Service Worker take control of pages ASAP
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+// let Workbox handle our precache list
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+// use `networkFirst` strategy for `*.html`, like all my posts
+workbox.routing.registerRoute(/\.html$/, workbox.strategies.networkFirst());
+
+// use `cacheFirst` strategy for images
+workbox.routing.registerRoute(/images/, workbox.strategies.cacheFirst());
+
+// third party files
+workbox.routing.registerRoute(
+  /^https?:\/\/maxcdn.bootstrapcdn.com/,
+  workbox.strategies.staleWhileRevalidate()
+);
